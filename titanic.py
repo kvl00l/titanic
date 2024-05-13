24ec5f606c5f6610f0ba4601201e533539871752
kvl00l <g12460766@tdrive.thu.edu.tw>
2024-05-13 16:50:19 +0800

    New

---
+++ titanic.py
@@ -0,0 +1,60 @@
+#
+# Kevin
+# Titanic Practice
+#
+
+# Titanic
+import pandas as pd
+import matplotlib as plt
+
+# Load the dataset
+df = pd.read_csv('titanic.csv')
+
+# Inspect structure
+df.shape
+df.info()
+
+df.describe()
+
+df.isnull().sum()
+
+# Add missing values and add median in age column
+df1 = df.copy()
+df1["Age"] = df["Age"].mean()
+df1.isnull().sum()
+
+# Drop missing value in cabin and embarked
+df1 = df1.dropna()
+df1.isnull().sum()
+
+
+# Count by value
+df1['Sex'].value_counts()
+
+import seaborn as sns
+sns.countplot(x='Sex', data=df1, ) 
+plt.show()
+
+df1[['Sex']].plot.line()
+plt.title('Kevin')
+plt.show()
+
+# By group
+df1['Age'].mean()
+
+# By group
+df.groupby(['Pregnancies', 'Outcome']).mean()
+df.groupby(['Pregnancies', 'Outcome']).value_counts()
+df.groupby(['Pregnancies', 'Outcome']).value_counts(normalize = True)
+
+# Improt plt 
+import matplotlib.pyplot as plt
+
+# Visual BMI
+df[['BMI', 'Glucose']].plot.line()
+plt.title('Kevin')
+plt.show()
+
+
+df[['BMI', 'Glucose']].plot.line(figsize=(20, 10), color={"BMI": "red", "Glucose": "blue"})
+plt.show()
